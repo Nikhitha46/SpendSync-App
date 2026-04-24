4e4feb938c3b5748b5851365da0c7868343fbb02
Nikhitha Kolli <nikhithakolli@Nikhithas-MacBook-Pro.local>
2026-04-24 16:08:02 +0530

    Converted SpendSync into PWA mobile app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.png'],
+      manifest: {
+        name: 'SpendSync',
+        short_name: 'SpendSync',
+        description: 'Daily Expense Tracker',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
